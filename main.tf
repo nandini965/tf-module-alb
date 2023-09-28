ce9761b9109cd7dee0e8d36edd03091ae3ccc505
nandini965 <nandhini965@gmail.com>
2023-09-28 10:18:06 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "http"
+    description      = "HTTP"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
